33fa7ac2a18a5c879cbb8007a8c21a8c43ed69e1
Mike James <michael.james6@gmail.com>
2020-06-08 09:15:30 +0100

    Reorganised

---
+++ TrainingDataSetBuilder/Program.cs
@@ -0,0 +1,49 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace TrainingDataSetBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Snippet> snippets = new List<Snippet>();
+
+            Console.WriteLine("Provide root directory");
+
+            var rootDir = Console.ReadLine();
+
+            if(!Directory.Exists(rootDir))
+                Console.WriteLine("Invalid Directory Path");
+
+            var dirInfo = new DirectoryInfo(rootDir);
+            foreach (var folder in dirInfo.GetDirectories())
+            {
+                var dirName = folder.Name;
+
+                foreach (var file in folder.GetFiles())
+                {
+                    var content = File.ReadAllText(file.FullName).Replace(Environment.NewLine, " ");
+
+                    if (!string.IsNullOrEmpty(content))
+                    {
+                        content.Trim();
+
+                        var snippet = new Snippet() { Language = dirName, Content = $"'{content}'"};
+                        snippets.Add(snippet);
+                    }
+                }
+            }
+
+            using (var writer = new StreamWriter($"{rootDir}\\merged.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(snippets);
+            }
+        }
+    }
+}
---
+++ TrainingDataSetBuilder/Snippet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingDataSetBuilder
+{
+    public class Snippet
+    {
+        public string Language { get; set; }
+        public string Content { get; set; }
+    }
+}
